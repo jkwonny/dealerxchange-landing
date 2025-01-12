8c09fdb6f7de23ce3ed1fdc23994c566e83e517b
jkwonny <justinkwon123@gmail.com>
2025-01-12 17:45:37 -0500

    Link

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,8 +13,8 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: 'DealerXChange',
-  description: 'DealerXChange.com',
+  title: 'DealerXChangeNetwork',
+  description: 'DealerXChangeNetwork.com',
 };
 
 export default function RootLayout({
